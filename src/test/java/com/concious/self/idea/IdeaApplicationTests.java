1b1357e2ea2f9372a3a82025e15f01740af1c36b
vg7venu <vg7venu@gmail.com>
2022-12-25 18:47:16 +0530

    feat: init

---
+++ src/test/java/com/concious/self/idea/IdeaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.concious.self.idea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
